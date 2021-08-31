a71799b4807e8b13fe3a21ca20f8f00892eb923c
osama.omar <osama@shgardi.app>
2021-08-31 12:23:42 +0200

    -add toast message .

--- app/src/main/java/com/example/testappdistrp/MainActivity.kt
+++ app/src/main/java/com/example/testappdistrp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.testappdistrp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"Hellllo",Toast.LENGTH_SHORT).show()
     }
 }
